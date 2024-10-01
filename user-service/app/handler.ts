480762fcbd5870192197613ec9020dc923da0490
Aimberê Rocha Mendonça <aimbererm@gmail.com>
2024-09-30 23:57:18 -0300

    feat: features

--- user-service/app/handler.ts
+++ user-service/app/handler.ts
@@ -4,39 +4,39 @@ import { RegisterUserDTO } from './dtos/user/register-user.dto';
 import { badRequest, pageNotFound } from './protocols/errors';
 import { instanceOfRegisterUser } from './instances/user/instance-register-user';
 
-// export const signup = async (event: APIGatewayProxyEventV2): Promise<HttpResponse> => {
-//   if (!event.body) {
-//     return badRequest([{
-//       property: 'body',
-//       constraints: {
-//         isRequired: 'Body is required'
-//       }
-//     }]);
-//   }
+export const signup = async (event: APIGatewayProxyEventV2): Promise<HttpResponse> => {
+  if (!event.body) {
+    return badRequest([{
+      property: 'body',
+      constraints: {
+        isRequired: 'Body is required'
+      }
+    }]);
+  }
 
-//   const controller = instanceOfRegisterUser();
-//   const registerUserData: RegisterUserDTO = JSON.parse(event.body);
+  const controller = instanceOfRegisterUser();
+  const registerUserData: RegisterUserDTO = JSON.parse(event.body);
 
-//   const dto = Object.assign(new RegisterUserDTO(), registerUserData);
+  const dto = Object.assign(new RegisterUserDTO(), registerUserData);
 
-//   return await controller.handle(dto);
-// };
+  return await controller.handle(dto);
+};
 
-// export const login = (event: APIGatewayProxyEventV2) => {
-//   console.log('event', event);
-//   return {
-//     statusCode: 200,
-//     body: JSON.stringify({ message: 'Hello World' }),
-//   };
-// };
+export const login = (event: APIGatewayProxyEventV2) => {
+  console.log('event', event);
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: 'Hello World' }),
+  };
+};
 
-// export const verify = (event: APIGatewayProxyEventV2) => {
-//   console.log('event', event);
-//   return {
-//     statusCode: 200,
-//     body: JSON.stringify({ message: 'Hello World' }),
-//   };
-// };
+export const verify = (event: APIGatewayProxyEventV2) => {
+  console.log('event', event);
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: 'Hello World' }),
+  };
+};
 
 export const profile = async (event: APIGatewayProxyEventV2) => {
   const httpMethod = event.requestContext.http.method;
