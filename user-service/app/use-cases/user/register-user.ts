1caaaba6d852cb0fbeb36e37a21f4477e7a5031d
Aimberê Rocha Mendonça <aimbererm@gmail.com>
2024-10-01 14:04:15 -0300

    feat: cognito access manager

--- user-service/app/use-cases/user/register-user.ts
+++ user-service/app/use-cases/user/register-user.ts
@@ -1,5 +1,6 @@
 import { RegisterUserDTO } from '../../dtos/user/register-user.dto';
 import { User } from '../../models/IUser';
+import { IAccessManager } from '../../providers/access-manager';
 import { IMailer } from '../../providers/mailer';
 import { IPasswordEncryptor } from '../../providers/password-encryptor';
 import { ISalt } from '../../providers/salt';
@@ -8,17 +9,12 @@ import { IUserRepository } from '../../repositories/user.repo';
 export class RegisterUserUseCase {
 
   constructor(
-    private readonly passwordEncryptor: IPasswordEncryptor,
-    private readonly genSalt: ISalt,
     private readonly userRepository: IUserRepository,
     private readonly mailer: IMailer,
+    private readonly accessManager: IAccessManager, 
   ) {}
 
   async execute(registerUserDto: RegisterUserDTO): Promise<User> {
-    const salt = await this.genSalt.generateSalt();
-    const hashedPassword = await this.passwordEncryptor.encrypt(registerUserDto.password, salt);
-    registerUserDto.password = hashedPassword;
-    registerUserDto.salt = salt;
 
     const user = this.userRepository.register(registerUserDto);
     this.mailer.sendMail(registerUserDto.email, 'Welcome message', 'Welcome to our platform');
