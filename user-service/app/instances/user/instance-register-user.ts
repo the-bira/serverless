b474e224cbf66bc3a4382910ca7f97c9d991c094
Aimberê Rocha Mendonça <aimbererm@gmail.com>
2024-10-03 16:16:20 -0300

    fix: update from house

--- user-service/app/instances/user/instance-register-user.ts
+++ user-service/app/instances/user/instance-register-user.ts
@@ -1,24 +1,22 @@
-import { RegisterUserController } from '../../http/user/register-user.controller';
-import { PrismaUserRepository } from '../../repositories/prisma-repositories/user.repo';
-import { RegisterUserUseCase } from '../../use-cases/user/register-user';
-import { BCryptPasswordEncryptor } from '../../utils/bcrypt-password-encryptor';
-import { BCryptSalt } from '../../utils/bcrypt-salt';
-import { ResendMailer } from '../../utils/resend-mailer';
-import { instanceOfPrismaService } from '../instance-prisma-service';
+import { RegisterUserController } from "../../http/user/register-user.controller";
+import { PrismaUserRepository } from "../../repositories/prisma-repositories/user.repo";
+import { RegisterUserUseCase } from "../../use-cases/user/register-user";
+import { CognitoAuth } from "../../utils/cognito-auth";
+import { ResendMailer } from "../../utils/resend-mailer";
+import { instanceOfPrismaService } from "../instance-prisma-service";
 
 export const instanceOfRegisterUser = (): RegisterUserController => {
-
-  const instanceOfPasswordEcryptor = new BCryptPasswordEncryptor();
-  const instanceOfGenSalt = new BCryptSalt();
+  const instanceOfAuthProvider = new CognitoAuth();
   const instanceOfResendMailer = new ResendMailer();
-  const instanceOfUserRepository = new PrismaUserRepository(instanceOfPrismaService());
+  const instanceOfUserRepository = new PrismaUserRepository(
+    instanceOfPrismaService()
+  );
 
   const instanceOfRegisterUserUseCase = new RegisterUserUseCase(
-    instanceOfPasswordEcryptor,
-    instanceOfGenSalt,
+    instanceOfAuthProvider,
     instanceOfUserRepository,
     instanceOfResendMailer
   );
 
-  return new RegisterUserController( instanceOfRegisterUserUseCase );
-}
+  return new RegisterUserController(instanceOfRegisterUserUseCase);
+};
--- user-service/app/use-cases/user/register-user.ts
+++ user-service/app/use-cases/user/register-user.ts
@@ -1,23 +1,36 @@
-import { RegisterUserDTO } from '../../dtos/user/register-user.dto';
-import { User } from '../../models/IUser';
-import { IAccessManager } from '../../providers/access-manager';
-import { IMailer } from '../../providers/mailer';
-import { IPasswordEncryptor } from '../../providers/password-encryptor';
-import { ISalt } from '../../providers/salt';
-import { IUserRepository } from '../../repositories/user.repo';
+import { RegisterUserDTO } from "../../dtos/user/register-user.dto";
+import { User } from "../../models/IUser";
+import { IAuthProvider } from "../../providers/auth";
+import { IMailer } from "../../providers/mailer";
+import { IPasswordEncryptor } from "../../providers/password-encryptor";
+import { ISalt } from "../../providers/salt";
+import { IUserRepository } from "../../repositories/user.repo";
 
 export class RegisterUserUseCase {
-
   constructor(
+    private readonly authProvider: IAuthProvider,
     private readonly userRepository: IUserRepository,
-    private readonly mailer: IMailer,
-    private readonly accessManager: IAccessManager, 
+    private readonly mailer: IMailer
   ) {}
 
   async execute(registerUserDto: RegisterUserDTO): Promise<User> {
+    const signUpUser = await this.authProvider.register(
+      registerUserDto.email,
+      registerUserDto.password!,
+      registerUserDto.phone
+    );
 
-    const user = this.userRepository.register(registerUserDto);
-    this.mailer.sendMail(registerUserDto.email, 'Welcome message', 'Welcome to our platform');
+    const user = this.userRepository.register({
+      email: registerUserDto.email,
+      phone: registerUserDto.phone,
+      cognitoId: signUpUser.id,
+    });
+
+    this.mailer.sendMail(
+      registerUserDto.email,
+      "Welcome message",
+      "Welcome to our platform"
+    );
 
     return user;
   }
